a3784c0028ef638698fc4fa95dd19785898c455a
tanner <tanner@tanner.ninja>
2019-11-06 18:23:08 -0500

    formatting

--- src/wifi_localization/win_wifi_test.py
+++ src/wifi_localization/win_wifi_test.py
@@ -4,8 +4,10 @@ stuff = WinWiFi.scan()
 thing = 1
 for item in stuff:
     print(item.ssid)
+    count = 1
     for bssid in item.bssids.keys():
-        print("{} - {}".format(bssid, item.bssids[bssid]))
+        print("  {} - {} - {}".format(count, bssid, item.bssids[bssid]))
+        count += 1
     # print("{}: {}".format(item.ssid, item.strength))
 
 
