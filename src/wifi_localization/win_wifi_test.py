4e32956dcd045ca568761bb1263613c0615df4de
tanner <tanner@tanner.ninja>
2019-11-07 09:48:54 -0500

    print RSSI also

--- src/wifi_localization/win_wifi_test.py
+++ src/wifi_localization/win_wifi_test.py
@@ -6,7 +6,7 @@ for item in stuff:
     print(item.ssid)
     count = 1
     for bssid in item.bssids.keys():
-        print("  {} - {} - {}".format(count, bssid, item.bssids[bssid]))
+        print("  {} - {} - {} - {}".format(count, bssid, item.bssids[bssid], ((item.bssids[bssid] / 2) - 100)))
         count += 1
     # print("{}: {}".format(item.ssid, item.strength))
 
