8e5f89addef9ae7dd0a1a8a5386911d3895f905a
tanner <tanner@tanner.ninja>
2019-11-30 12:37:50 -0500

    scan and save wifi data

--- src/raspberrypi/test.py
+++ src/raspberrypi/test.py
@@ -1,18 +1,25 @@
 import datetime
-import time
 import iwlist
 import json
-
-scan_count = 10
+import os
+# todo kalman filters https://www.wouterbulten.nl/blog/tech/kalman-filters-explained-removing-noise-from-rssi-signals/
+scan_count = 500
 print("scanning {} times...".format(scan_count))
 data = []
+empty_cell_data_count = 0
 for i in range(scan_count):
-    time.sleep(1)
-    content = iwlist.scan(interface='wlan0')
-    cells = {"time": datetime.datetime.now(), "cells": iwlist.parse(content)}
-    data.append(cells)
-    with open('data.json', 'wb') as fp:
-        json.dump(data, fp, sort_keys=True, indent=4, default=str)
-        print("scan {} complete. saved data to data.json".format(i))
-
-
+    timestamp = datetime.datetime.now()
+    content = iwlist.scan(interface='wlan0')  # takes 3 or 4 seconds
+    cells = {"time": timestamp, "cells": iwlist.parse(content)}
+    # save each scan to its own file, the filename being the timestamp
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    filename = "data/{:%Y%m%d_%H_%M_%S.%f}_scan{}.json".format(timestamp, i)
+    with open(filename, 'wb') as fp:
+        json.dump(cells, fp, sort_keys=True, indent=4, default=str)
+        if cells is None or len(cells) < 1:
+            empty_cell_data_count += 1
+            print("scan {} failed, no cell data. saved data to {}".format(i, filename))
+        else:
+            print("scan {} complete. saved data to {}".format(i, filename))
+print("scans with no data: {} of {}".format(empty_cell_data_count, scan_count))
---
+++ src/raspberrypi/test_datetimeformat.py
@@ -0,0 +1,3 @@
+import datetime
+
+print("{:%Y%m%d_%H_%M_%S.%f}".format(datetime.datetime.now()))
