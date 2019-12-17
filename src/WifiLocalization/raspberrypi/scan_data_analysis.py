a8361812860a043fc696a1d0cd3a757fe48818ba
tanner <tanner@tanner.ninja>
2019-12-17 12:24:45 -0500

    scan analysis for distance calculations

--- src/WifiLocalization/raspberrypi/scan_data_analysis.py
+++ src/WifiLocalization/raspberrypi/scan_data_analysis.py
@@ -1,25 +1,31 @@
 # run locally, not on the pi
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import colors
-from matplotlib.ticker import PercentFormatter
 import json
+import os
+import test
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-filename = ""
+scan_file = "C:\\github\\SeniorDesign\\Main-Repo\\src\\WifiLocalization\\raspberrypi\\scan_data\\20191217_17_05_15.224575_scan-1.json"# filename = ""
 raw = []
-with open(filename) as json_file:
+with open(scan_file) as json_file:
     data = json.load(json_file)
     for scan_group in data:
         for scan in scan_group["cells"]:
-            print(f"scan raw: {scan['distance']['raw']}")
-            raw.append(scan['distance']['raw'])
+            scan_distance = test.convert_rssi_to_meters(float(scan['signal_level_dBm']), float(scan['frequency']))
+            if scan_distance < 10:
+                # print(f"scan raw: {scan['distance']['raw']}")
+                raw.append(scan_distance)
 
-n_bins = 20
+# for filename in os.listdir(directory):
+#     with open(directory + "\\" + filename) as json_file:
+#         data = json.load(json_file)
+#         for scan in data["cells"]:
+#             if float(scan["frequency"]) < 5 and scan['distance']['raw'] < 10:
+#                 # print(f"scan raw: {scan['distance']['raw']}")
+#                 raw.append(scan['distance']['raw'])
 
+print(f"max: {max(raw)}\nmin: {min(raw)}\nrange: {max(raw) - min(raw)}")
+n_bins = 10
 n, bins, patches = plt.hist(raw, n_bins)
 plt.show()
 
-print(f"max: {max(raw)}\nmin: {min(raw)}\nrange: {max(raw) - min(raw)}")
+
